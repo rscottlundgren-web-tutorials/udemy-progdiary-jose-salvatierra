2a9afe2b7673ae20ae5293db487feed3ed2e7068
rscottlundgren <rscottlundgren@gmail.com>
2020-09-13 11:47:11 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+menu = """Please select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection: """
+welcome = "Welcome to the programming diary!"
+
+print(welcome)
+
+# := (Walrus Operator) => Should always have lower precedence than the comparison
+# While user_input which is equal to whether input(menu) is not equal to 3, then run following code
+while (user_input := input(menu)) != "3":
+    if user_input == "1":
+        print("Adding...")
+    elif user_input == "2":
+        print("Viewing...")
+    else:
+        print("Invalid option, please try again!")
